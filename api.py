99968d86e3f09b0553582ce0def558d11ab7943c
nguyenminh09072000 <65122860+nguyenminh09072000@users.noreply.github.com>
2022-07-04 07:19:26 +0700

    fix api

--- api.py
+++ api.py
@@ -1,13 +1,14 @@
 import cv2
-
+import time
 def detect_face(img, mtcnn):
     # try:
+        start= time.time()
         bboxs, scores = mtcnn.detect(img)
+        end = time.time()
+        print('MTCCN TIME: ', end - start)
         print('Detected',len(scores),'faces')
         for num ,(bbox, score) in enumerate(zip(bboxs, scores)):
             b = [int(bb) for bb in bbox]
-<<<<<<< HEAD
-            # face = img[b[1]:b[3],b[0]:b[2]]
             point1 =(b[0],b[1])
             point2 = (b[0], b[3])
             point3 = (b[2],b[3])
@@ -17,17 +18,17 @@ def detect_face(img, mtcnn):
             cv2.line(img, point3, point4, (255,255,0), 3)
             cv2.line(img, point4, point1, (255,255,0), 3)
 
-        cv2.imshow('face_'+str(num), img)
+        cv2.imshow('image',img)
         cv2.waitKey(0)
         cv2.destroyAllWindows()
-    except:
-        print('Can\'t detect face')
-=======
-            face = img[b[1]:b[3],b[0]:b[2]]
-            cv2.imshow('face_'+str(num), face)
-            cv2.waitKey(0)
-            cv2.destroyAllWindows()
     # except:
     #     print('Can\'t detect face')
+# =======
+#             face = img[b[1]:b[3],b[0]:b[2]]
+#             cv2.imshow('face_'+str(num), face)
+#             cv2.waitKey(0)
+#             cv2.destroyAllWindows()
+#     except:
+#         print('Can\'t detect face')
 
->>>>>>> 1bcff4621591fc421a52663e2fac1ee7aa1c264e
+# >>>>>>> 1bcff4621591fc421a52663e2fac1ee7aa1c264e
--- demo.py
+++ demo.py
@@ -11,12 +11,11 @@ import argparse
 if __name__ == '__main__':
     parser_ = argparse.ArgumentParser()
     parser_.add_argument('--device', type=str, help='example cpu, cuda:0, cuda:1,...', default='cpu')
-    parser_.add_argument('--path', type=str, help='path to image directory',default='image_example/img_1.jpg')
+    parser_.add_argument('--path', type=str, help='path to image directory',default='C:\\Users\\GS75\\PycharmProjects\\open_cv_MTCNN\\MTCNN\\image_example\\img_3.jpg')
     mtcnn = MTCNN()
     opt = parser_.parse_args()
     img = cv2.imread(opt.path)
-<<<<<<< HEAD
+
+
     detect_face(img, mtcnn)
-=======
-    detect_face(img, mtcnn)
->>>>>>> 1bcff4621591fc421a52663e2fac1ee7aa1c264e
+
