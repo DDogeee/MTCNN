c0b1590854967a01d7b6be22965a6551eae04b1a
nguyenminh09072000 <65122860+nguyenminh09072000@users.noreply.github.com>
2022-07-04 07:30:55 +0700

    update

--- api.py
+++ api.py
@@ -17,7 +17,7 @@ def detect_face(img, mtcnn):
             cv2.line(img, point3, point2, (255, 255, 0), 3)
             cv2.line(img, point3, point4, (255,255,0), 3)
             cv2.line(img, point4, point1, (255,255,0), 3)
-
+        img = cv2.resize(img,(800,600))
         cv2.imshow('image',img)
         cv2.waitKey(0)
         cv2.destroyAllWindows()
